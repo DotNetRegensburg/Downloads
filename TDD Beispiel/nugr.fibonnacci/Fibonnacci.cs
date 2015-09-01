7bd2e3f4fce5c3a9bd0dee18311c3ff99603a162
Florian Rappl <rappl.florian@gmail.com>
2015-09-01 22:59:37 +0200

    Added Demo

---
+++ TDD Beispiel/nugr.fibonnacci/Fibonnacci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace nugr.application
+{
+    public class Fibonnacci
+    {
+        public int Calculate(int n)
+        {
+            if (n == 0) return 0;
+	        if (n == 1) return 1;
+	        return Calculate(n-1) + Calculate(n-2);
+
+        }
+    }
+}
---
+++ TDD Beispiel/nugr.finbonnacci_test/TestFibonnacci.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using nugr.application;
+
+namespace nugr.application_test
+{
+    [TestFixture]
+    public class TestFibonnacci
+    {
+        private Fibonnacci _fibonnacci;
+
+        [SetUp]
+        public void Init()
+        {
+            this._fibonnacci = new Fibonnacci();
+        }
+
+        [Test]
+        public void Calculate_Fibonnacci()
+        {
+            int[][] cases ={new int[]{0,0}, new int[]{1,1}, new int[]{2,1}, 
+                new int[]{3,2}, new int[]{4,3}, new int[]{5,5}};
+
+	        for (int i = 0; i < cases.Length; i++)
+	        {
+                Assert.AreEqual(cases[i][1], _fibonnacci.Calculate(cases[i][0]));
+	        }
+        }
+    }
+}
